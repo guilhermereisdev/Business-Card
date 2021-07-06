b6265271f8358d857a28d59a3c42b7a3c4c21888
Guilherme Reis <guilhermereis2009@hotmail.com>
2021-07-05 22:31:19 -0300

    commit inicial

---
+++ app/build/tmp/kapt3/stubs/debug/br/com/guilhermereisapps/businesscard/ui/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.guilhermereisapps.businesscard.ui;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0013\u001a\u00020\u0014H\u0002J\b\u0010\u0015\u001a\u00020\u0014H\u0002J\u0012\u0010\u0016\u001a\u00020\u00142\b\u0010\u0017\u001a\u0004\u0018\u00010\u0018H\u0014R\u001b\u0010\u0003\u001a\u00020\u00048BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0007\u0010\b\u001a\u0004\b\u0005\u0010\u0006R\u001b\u0010\t\u001a\u00020\n8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\r\u0010\b\u001a\u0004\b\u000b\u0010\fR\u001b\u0010\u000e\u001a\u00020\u000f8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\u0012\u0010\b\u001a\u0004\b\u0010\u0010\u0011\u00a8\u0006\u0019"}, d2 = {"Lbr/com/guilhermereisapps/businesscard/ui/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "adapter", "Lbr/com/guilhermereisapps/businesscard/ui/BusinessCardAdapter;", "getAdapter", "()Lbr/com/guilhermereisapps/businesscard/ui/BusinessCardAdapter;", "adapter$delegate", "Lkotlin/Lazy;", "binding", "Lbr/com/guilhermereisapps/businesscard/databinding/ActivityMainBinding;", "getBinding", "()Lbr/com/guilhermereisapps/businesscard/databinding/ActivityMainBinding;", "binding$delegate", "mainViewModel", "Lbr/com/guilhermereisapps/businesscard/ui/MainViewModel;", "getMainViewModel", "()Lbr/com/guilhermereisapps/businesscard/ui/MainViewModel;", "mainViewModel$delegate", "getAllBusinessCard", "", "insertListener", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+    private final kotlin.Lazy binding$delegate = null;
+    private final kotlin.Lazy mainViewModel$delegate = null;
+    private final kotlin.Lazy adapter$delegate = null;
+    
+    private final br.com.guilhermereisapps.businesscard.databinding.ActivityMainBinding getBinding() {
+        return null;
+    }
+    
+    private final br.com.guilhermereisapps.businesscard.ui.MainViewModel getMainViewModel() {
+        return null;
+    }
+    
+    private final br.com.guilhermereisapps.businesscard.ui.BusinessCardAdapter getAdapter() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void insertListener() {
+    }
+    
+    private final void getAllBusinessCard() {
+    }
+    
+    public MainActivity() {
+        super();
+    }
+}
---
+++ app/src/main/java/br/com/guilhermereisapps/businesscard/ui/MainActivity.kt
@@ -0,0 +1,42 @@
+package br.com.guilhermereisapps.businesscard.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.guilhermereisapps.businesscard.App
+import br.com.guilhermereisapps.businesscard.databinding.ActivityMainBinding
+import br.com.guilhermereisapps.businesscard.util.Image
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
+        insertListener()
+    }
+
+    private fun insertListener() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCard ->
+            adapter.submitList(businessCard)
+        }
+    }
+}
